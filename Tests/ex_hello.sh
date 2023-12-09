d039398e5a83eb8d7feef43f6684c4cb65a1748b
Younes Oubellal <108194849+mr-youbella@users.noreply.github.com>
2023-12-09 04:25:14 +0100

    Add Message to error

--- Tests/ex_hello.sh
+++ Tests/ex_hello.sh
@@ -1,6 +1,7 @@
 gcc -w -fno-diagnostics-show-caret ../../ft_hello.c -o ft_hello
 
 if [ $? != 0 ]; then
+    echo -e "\033[31m\n--------------------------------------------------------------------------------------\nThere seems to be an error: Please check the exercise file in terms of Syntax.\n\033[0m"
     exit
 fi
 op="$(./ft_hello)"
